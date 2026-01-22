1be9ef91fdd9e7e1bf2bdee91ba8eee73a3d0da2
JulyShev <uliasevcuk1452@gmail.com>
2026-01-22 22:32:54 +0500

    lesson3

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -1,4 +1,31 @@
 package manager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
 public class AppManager {
+    private WebDriver driver;
+    public WebDriver getDriver(){ // getter для того чтобы использовать driver в классах который наследуют AppManager
+        return driver;
+    }
+    @BeforeMethod
+    public void setUp(){ //What we do before @Test. Here we are writing settings to our test.
+                         // (initialisation of browser, open window to max etc)
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    // (@BeforeMethod) setup --> (@Test) testName --> (@AfterMethod) tearDown
+
+    @AfterMethod (enabled = false)
+    public void tearDown(){ //What we do after tests?
+        if(driver != null){
+            driver.quit(); // close the window of Chrome.
+        }
+    }
 }
--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,4 +1,33 @@
 package ui_tests;
 
-public class LoginTests {
+import dto.User;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends AppManager {
+    @Test
+    public void logInPositiveTest(){
+        System.out.println("first test");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegForm("family@mail.ru",
+                "Family123!");
+        loginPage.clickBtnLoginForm();
+    }
+
+    @Test
+    public void loginPositiveTestWithUser(){
+        User user = new User("family@mail.ru",
+                "Family123!");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+    }
 }
